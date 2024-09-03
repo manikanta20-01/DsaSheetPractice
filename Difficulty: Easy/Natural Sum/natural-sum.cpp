b11583fea2e594063f993fd417ef8b63f6536586
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2024-09-03 09:20:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Natural Sum/natural-sum.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+   	int find(int n){
+   	    // Code here
+   	    
+   	    if(n <= 1)
+   	    {
+   	        return n;
+   	        
+   	    }
+   	    
+   	    int sum = 0;
+   	    for(int i = 1;i < n;i++)
+   	    {
+   	        sum += i;
+   	        if(sum == n)
+   	        return i;
+   	    }
+   	    return -1;
+   	    
+   	    
+   	    
+   	}    
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.find(n);
+		cout << ans <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
