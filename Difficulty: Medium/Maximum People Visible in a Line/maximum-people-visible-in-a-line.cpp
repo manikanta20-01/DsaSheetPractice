6f35e5121011de6b20b5a41323edcd4b2e64aaaf
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2026-01-23 07:20:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum People Visible in a Line/maximum-people-visible-in-a-line.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxPeople(vector<int> &arr) {
+        // code here
+          int n=arr.size();
+        int res=1;
+        vector<int> maxarr={-1};
+        int maxi = *max_element(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            if(arr[i]==maxi)
+                maxarr.push_back(i);
+        }
+        maxarr.push_back(n);
+        for(int i=0;i<maxarr.size()-1;i++)
+            res=max(res,maxarr[i+1]-maxarr[i-1]-1);
+        return res;
+    }
+};
