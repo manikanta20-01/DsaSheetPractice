4269f0367f4c471c78615aef3b23334b07e60c46
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2026-05-12 14:07:14 +0530

    Time: 27 ms (95.15%), Space: 97.3 MB (63.11%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+
+        int answer = 0;
+        int energy = 0;
+
+        for (auto& task : tasks) {
+
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (energy < minimum) {
+
+                int need = minimum - energy;
+
+                answer += need;
+                energy += need;
+            }
+
+            energy -= actual;
+        }
+
+        return answer;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
