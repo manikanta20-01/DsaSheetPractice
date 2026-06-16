7edb035466b89aba86bcef0fc8d2f964f602d768
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2026-06-16 19:02:01 +0530

    Time: 3 ms (18.3%), Space: 9.1 MB (24.01%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int depth = 0;
+        string ans;
+
+        for(char ch : s) {
+            if(ch == '(') {
+                if(depth > 0) ans += ch;
+                depth++;
+            } else {
+                depth--;
+                if(depth > 0) ans += ch;
+            }
+        }
+        return ans;
+    }
+};
