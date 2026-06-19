38bd67076b24c6182c372895c11e000e48182a69
Namo manikanta Sai <122387885+manikanta20-01@users.noreply.github.com>
2026-06-19 07:18:09 +0530

    Time: 0 ms (100%), Space: 10.9 MB (21%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        for(int i = 1;i < n;i++) {
+            gain[i] += gain[i - 1];
+        }
+        gain.push_back(0);
+        return ranges::max(gain);
+    }
+};
